1dbc6b0a1168dc84d42181dcdf6cb64075dfce69
Dinidu Sachintha <dinidusachintha3@gmail.com>
2024-12-23 17:19:10 +0530

    Home Page 2024-12-23 17-19-10 @Dinidu21

--- obj/Debug/net9.0/InvenFlow-MVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/InvenFlow-MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvenFlow-MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fc17541b3cbd7a8a4d6841da880ff179fd31b54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e7d5728b770a890d0a54b8e2fe07095f6ae0fd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvenFlow-MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvenFlow-MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
